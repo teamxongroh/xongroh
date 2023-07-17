7d9068590218f6640964022bff23e2754fb5181a
phukon <rikiphukon16@gmail.com>
2023-07-18 00:23:17 +0530

    cors update

--- xongroh-backend/config/allowedOrigins.js
+++ xongroh-backend/config/allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3500',
     'https://xongroh.vercel.app/',
+    'http://localhost:8000/'
 ]
 export default allowedOrigins;
