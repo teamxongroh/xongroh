732e047819f03043f01dfd2883d3406044e889bd
phukon <rikiphukon16@gmail.com>
2023-07-21 00:17:15 +0530

    bug fixes and corrected routing

--- xongroh-backend/routes/userRoute.js
+++ xongroh-backend/routes/userRoute.js
@@ -14,10 +14,14 @@ router.route('/authenticate').post(userController.verifyUser, (req, res) => res.
 router.route('/login').post(rateLimiter, userController.verifyUser,userController.login); // login in app
 
 /** GET Methods */
-router.route('/:username').get(userController.getUser) // user with username
 router.route('/generateOTP').get(userController.verifyUser, localVariables, userController.generateOTP) // generate random OTP
 router.route('/verifyOTP').get(userController.verifyUser, userController.verifyOTP) // verify generated OTP
 router.route('/createResetSession').get(userController.createResetSession) // reset all the variables
+router.route('/:username').get(userController.getUser) // user with username
+// In Express, routes are matched in the order they are declared. When you have a route with a
+// parameter (e.g., /:username) followed by a route without any parameters (e.g., /generateOTP), 
+// the second route (/generateOTP) will be treated as if it matches any string, including the
+// one that should have been handled by the first route (/:username).
 
 
 /** PUT Methods */
