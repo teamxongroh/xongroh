e1db4a4aa098af73788193e4399b359f451ab021
phukon <rikiphukon16@gmail.com>
2023-08-01 21:22:53 +0530

    new project

--- xongroh-frontend/vite.config.js
+++ xongroh-frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
+
+  // for absolute paths
+  resolve: {
+    alias: {
+      '@' : path.resolve(__dirname, './src/')
+    }
+  }
 })
