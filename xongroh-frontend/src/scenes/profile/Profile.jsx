4c3c71bfa6b37dfd0e71c0c7b3a9512b927da30c
phukon <rikiphukon16@gmail.com>
2023-08-23 15:22:22 +0530

    profile update

--- xongroh-frontend/src/scenes/profile/Profile.jsx
+++ xongroh-frontend/src/scenes/profile/Profile.jsx
@@ -9,11 +9,11 @@ const Profile = () => {
           <div className='header px-3 pt-5 pb-5 bg-[#F0D9FF] rounded-b-3xl flex flex-col items-center'>
               <div className='flex flex-row'>
                 <div>25 creations</div>
-                <img src={Assets.user2}/>
+                <img src={Assets.user2} className='relative bottom-16'/>
                 <div>25k Supporters</div>
               </div>
-              <div>Mahindra alpha beta</div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis a est dapibus posuere. Donec molestie risus ac elit imperdiet pretium. Phasellus volutpat nec ligula at accumsan.</p>
+              <div className='font-bold'>Mahindra alpha beta</div>
+              <p className=' px-5 py-5 text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis a est dapibus posuere. Donec molestie risus ac elit imperdiet pretium. Phasellus volutpat nec ligula at accumsan.</p>
               <div className='flex flex-row'>
                   <button className='border-2 border-black rounded-3xl  bg-[#F5F5F7]'>Creators</button>
                   <button className='border-2 border-black rounded-3xl  bg-[#F5F5F7]'>Communities</button>
