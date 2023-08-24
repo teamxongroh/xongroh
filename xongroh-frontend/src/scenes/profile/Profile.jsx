4c8ccfcb4e55b9e1990d0457dcbfaa3cd6c86f46
rupambora31 <iamrupam99@gmail.com>
2023-08-24 09:41:41 +0530

    refined-css-of-profile-section

--- xongroh-frontend/src/scenes/profile/Profile.jsx
+++ xongroh-frontend/src/scenes/profile/Profile.jsx
@@ -6,36 +6,43 @@ const Profile = () => {
       <div className='bg-[#FFDEB4]'>
         <div className='flex flex-col'>
           <img src={Assets.dummy1} className=' h-40 object-cover'/>
-          <div className='header px-3 pt-5 pb-5 bg-[#F0D9FF] rounded-b-3xl flex flex-col items-center'>
-              <div className='flex flex-row -mb-10'>
-                <div>25 creations</div>
+          <div className='header px-3 pt-5 pb-6 bg-[#F0D9FF] rounded-b-3xl flex flex-col items-center'>
+              <div className='w-full flex flex-row -mb-10 justify-around'>
+                <div className='text-center'>
+                  <div className='font-bold'>25</div>
+                  <div>Creations</div>
+                </div>
                 <img src={Assets.user2} className='relative bottom-16'/>
-                <div>25k Supporters</div>
+                <div className='text-center'>
+                  <div className='font-bold'>25k</div>
+                  <div>Supporters</div>
+                </div>
               </div>
               
-              <div className='font-bold pb-2'>Mahindra alpha beta</div>
+              <div className='font-bold text-lg pt-2 pb-2'>Mahindra Alpha Beta</div>
               <p className=' px-5 text-center pb-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis a est dapibus posuere. Donec molestie risus ac elit imperdiet pretium. Phasellus volutpat nec ligula at accumsan.</p>
               
-              <div className='flex flex-row justify-between'>
-                  <button className='border-2 px-3 mx-3 border-black rounded-3xl bg-[#F5F5F7]'>Support</button>
-                  <button className='border-2 px-3 mx-3 border-black rounded-3xl bg-[#C8E4B2]'>Message</button>
-                  <button className='border-2 px-3 mx-3 border-black rounded-3xl bg-[#F5F5F7]'>Events</button>
+              <div className='w-full flex justify-center flex-row'>
+                  <button className='border-2 px-3 py-0.5 mx-4 border-black rounded-3xl bg-[#F5F5F7]'>Support</button>
+                  <button className='border-2 px-3 py-0.5 mx-4 border-black rounded-3xl bg-[#C8E4B2]'>Message</button>
+          
               </div>
           </div>
         </div>
 
-        <div className='p-5'>
+        <div className='p-2 pt-6 pb-2'>
           <div className='overflow-x-scroll whitespace-nowrap'>
-              <div className='flex flex-row justify-between font-semibold'>
-                  <button className='border-2 border-black rounded-3xl px-3 mx-2 bg-[#F5F5F7]'>Creators</button>
-                  <button className='border-2 border-black rounded-3xl px-3 mx-2 bg-[#F5F5F7]'>Communities</button>
-                  <button className='border-2 border-black rounded-3xl px-3 mx-2 bg-[#F5F5F7]'>Events</button>
-                  <button className='border-2 border-black rounded-3xl px-3 mx-2 bg-[#F5F5F7]'>Courses</button>
+              <div className='w-full justify-around flex flex-row font-semibold'>
+                  <button className='border-2 border-black rounded-3xl px-3 py-0.5 bg-[#F5F5F7] mr-2'>Portfolio</button>
+                  <button className='border-2 border-black rounded-3xl px-3 py-0.5 bg-[#F5F5F7] mr-2'>Tribe</button>
+                  <button className='border-2 border-black rounded-3xl px-3 py-0.5 bg-[#F5F5F7] mr-2'>Store</button>
+                  <button className='border-2 border-black rounded-3xl px-3 py-0.5 bg-[#F5F5F7]'>Events</button>
+                  
               </div>
           </div>
         </div>
 
-      <div className='px-3'>
+      <div className='px-4'>
         <ImageDiv/>
         <ImageDiv/>
         <ImageDiv/>
