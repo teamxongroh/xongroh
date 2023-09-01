9864018c15f787ce6817f1d3046a67fccf56799c
rupambora31 <iamrupam99@gmail.com>
2023-09-01 23:03:13 +0530

    ProfilePost style update

--- xongroh-frontend/src/components/ProfilePost.jsx
+++ xongroh-frontend/src/components/ProfilePost.jsx
@@ -12,10 +12,10 @@ const ProfilePost = () => {
     <div className="overflow-hidden">
       <div>
         <div
-          className="mt-5 flex h-60 w-full flex-row justify-between rounded-t-3xl border-2 border-black"
+          className="mt-5 flex h-60 w-full flex-row justify-between rounded-t-2xl "
           style={divStyle}
         >
-          <div className="ml-2 mt-3 flex h-[15%] w-[50%] items-center justify-evenly rounded-3xl border-2 border-black bg-[#FDCEDF] py-1.5 text-center font-semibold">
+          <div className="ml-3 mt-3 flex h-[15%] w-[50%] items-center justify-evenly rounded-3xl shadow-button bg-[#DFCCFB] py-1.5 text-center font-semibold">
             <div>
               <img
                 className="h-8 w-8 rounded-full "
@@ -25,13 +25,13 @@ const ProfilePost = () => {
             </div>
             <div className="text-sm font-semibold">Mahendra Alpha</div>
           </div>
-          <button className="mr-2 mt-3 h-9 w-20 rounded-3xl border-2 border-black bg-[#FDCEDF] font-medium ">
+          <button className="mr-3 mt-3 h-9 w-20 rounded-3xl shadow-button bg-[#DFCCFB] font-medium ">
             100+
           </button>
         </div>
       </div>
 
-      <div className="w-100 flex h-36 flex-row items-center rounded-b-3xl border-x-2 border-b-2 border-black bg-[#C1EFFF] pb-2 pl-1 font-semibold">
+      <div className="w-100 flex h-36 flex-row items-center rounded-b-2xl bg-[#C8E4B2] pb-2 pl-1 font-semibold">
         <div className="items-center">
           <div className="line-clamp-2 pl-3 pr-4 text-sm font-bold ">
             lorem ipsum lorem ipsum aru kiba kibi lorem ipsum lorem ipsum aru
