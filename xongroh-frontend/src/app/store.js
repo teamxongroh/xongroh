f9d87ac341ddb2ee3e8de6851c4c20bf5486551b
phukon <rikiphukon16@gmail.com>
2023-09-02 11:17:32 +0530

    setup listeners

--- xongroh-frontend/src/app/store.js
+++ xongroh-frontend/src/app/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from '../features/api/apiSlice';
+import { setupListeners } from "@reduxjs/toolkit/query"
+
 
 export const store = configureStore({
     reducer: {
@@ -9,3 +11,5 @@ export const store = configureStore({
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
 })
+
+setupListeners(store.dispatch)
