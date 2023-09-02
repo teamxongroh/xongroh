6f3865cfba84a0c8662ceb25e5020fbab2df5a5a
rupambora31 <iamrupam99@gmail.com>
2023-09-02 20:18:18 +0530

    added CommunityPage route

--- xongroh-frontend/src/App.jsx
+++ xongroh-frontend/src/App.jsx
@@ -8,32 +8,29 @@ import MyCommunities from "@/scenes/communities/MyCommunities";
 import CommunityPage from "@/scenes/communities/CommunityPage";
 import Profile from "@/scenes/profile/Profile";
 import Layout from "@/scenes/Layout";
-import Public from "@/components/Public"
-import Prefetch from "@/features/auth/Prefetch"
-import DashLayout from "@/components/DashLayout"
-import Welcome from "@/features/auth/Welcome"
+import Public from "@/components/Public";
+import Prefetch from "@/features/auth/Prefetch";
+import DashLayout from "@/components/DashLayout";
+import Welcome from "@/features/auth/Welcome";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Public/>}/>
-        <Route path="login" element={<LoginPage />}/>
-        <Route path= 'register' element= {<RegisterPage/>} />
+        <Route index element={<DashLayout />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
 
-        <Route element={<Prefetch/>}>
-
-          <Route path="dash" element={<DashLayout/>}>
-
-            <Route index element={<Welcome/>} />
+        <Route element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
 
             <Route path="home" element={<HomePage />} />
             <Route path="search" element={<SearchPage />} />
             <Route path="communities" element={<MyCommunities />} />
             <Route path="profile" element={<Profile />} />
-            
+            <Route path="communitypage" element={<CommunityPage />} />
           </Route>
-
         </Route>
       </Route>
     </Routes>
