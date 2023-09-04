e167b5eba80d4a772cef5d22ad48e26205017505
phukon <rikiphukon16@gmail.com>
2023-09-04 21:39:17 +0530

    login logout functionality

--- xongroh-frontend/src/components/DashHeader.jsx
+++ xongroh-frontend/src/components/DashHeader.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import Assets from '@/assets/Assets'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, Link, useLocation } from 'react-router-dom'
+import { useSendLogoutMutation } from '@/features/auth/authApiSlice'
+import { useEffect } from 'react'
 
 const DashHeader = ({ bgColor }) => {
+
+  const navigate = useNavigate()
+
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation()
+
+  useEffect(() => {
+    if (isSuccess) navigate('/')
+  }, [isSuccess, navigate])
+
+  if (isLoading) return <p>Logging Out...</p>
+
+  if (isError) return <p>Error: {error.data?.message}</p>
+
   const defaultBgColor = '#F5F0BB'
 
   return (
@@ -15,6 +34,9 @@ const DashHeader = ({ bgColor }) => {
         <div className="flex flex-row space-x-5">
           <img className="h-8 w-8" src={Assets.bell} alt="notification" />
           <img className="h-8 w-8" src={Assets.chat} alt="chat" />
+          <button className="icon-button" title="Logout" onClick={sendLogout}>
+            <FontAwesomeIcon icon={faRightFromBracket} size="2x" />
+          </button>
         </div>
       </div>
     </div>
