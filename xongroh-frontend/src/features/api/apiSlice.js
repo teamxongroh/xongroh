a3482d19cae233cd0a917323f0d2e75654a3b2cc
phukon <rikiphukon16@gmail.com>
2023-09-06 06:19:33 +0530

    store reconfig

--- xongroh-frontend/src/features/api/apiSlice.js
+++ xongroh-frontend/src/features/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-  reducerPath: 'api', // optional
+  reducerPath: 'api-version1',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/v1/' }),
   tagTypes: ['Post', 'User'],
   endpoints: (builder) => ({}),
