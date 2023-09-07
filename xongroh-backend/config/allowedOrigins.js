3c98f205d7fdcfcd165b20c3ab0b20bd4cf1b24c
phukon <rikiphukon16@gmail.com>
2023-09-07 15:09:59 +0530

    removed  origins

--- xongroh-backend/config/allowedOrigins.js
+++ xongroh-backend/config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3500',
-  'https://xongroh.vercel.app/',
-  'http://localhost:8000/',
   'https://xongroh-backend.onrender.com/',
   'https://teamxongroh.vercel.app/',
 ]
