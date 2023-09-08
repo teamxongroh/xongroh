9ae6ca70485aa9bd93c7e683bebe72d9addb6e12
phukon <rikiphukon16@gmail.com>
2023-09-08 20:57:40 +0530

    updated allowed origins

--- xongroh-backend/config/allowedOrigins.js
+++ xongroh-backend/config/allowedOrigins.js
@@ -3,5 +3,8 @@ const allowedOrigins = [
   'http://localhost:3500',
   'https://xongroh.vercel.app/',
   'https://xongroh-9xkm.vercel.app/',
+  'https://xongroh-9xkm-phukon.vercel.app/',
+  'https://xongroh-9xkm-git-main-phukon.vercel.app/',
+  'https://xongroh-9xkm-96up0osc2-phukon.vercel.app/',
 ]
 export default allowedOrigins
